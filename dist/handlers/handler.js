c2494efbc0f3361b3ad3229450b4260cfa308ae7
Mate Pankucsi <mate.pankucsi@gmail.com>
2020-01-02 00:51:11 +0100

    add dist

---
+++ dist/handlers/handler.js
@@ -0,0 +1,197 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var AbstractHandler = /** @class */ (function () {
+    function AbstractHandler() {
+    }
+    AbstractHandler.prototype.setNext = function (handler) {
+        this.nextHandler = handler;
+        return handler;
+    };
+    AbstractHandler.prototype.handle = function (range, range2) {
+        if (this.nextHandler) {
+            return this.nextHandler.handle(range, range2);
+        }
+        return null;
+    };
+    return AbstractHandler;
+}());
+exports.AbstractHandler = AbstractHandler;
+var between = function (x, min, max) {
+    return x >= min && x <= max;
+};
+var SameRangeHandler = /** @class */ (function (_super) {
+    __extends(SameRangeHandler, _super);
+    function SameRangeHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    SameRangeHandler.prototype.isSuitable = function (range, range2) {
+        return range.start === range2.start && range.end === range2.end;
+    };
+    SameRangeHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2) ? [] : _super.prototype.handle.call(this, range, range2);
+    };
+    return SameRangeHandler;
+}(AbstractHandler));
+exports.SameRangeHandler = SameRangeHandler;
+var StartWithItHandler = /** @class */ (function (_super) {
+    __extends(StartWithItHandler, _super);
+    function StartWithItHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    StartWithItHandler.prototype.isSuitable = function (range, range2) {
+        return range.start === range2.start && between(range2.end, range.start, range.end);
+    };
+    StartWithItHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range2.end,
+                    end: range.end,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return StartWithItHandler;
+}(AbstractHandler));
+exports.StartWithItHandler = StartWithItHandler;
+var EndWithItHandler = /** @class */ (function (_super) {
+    __extends(EndWithItHandler, _super);
+    function EndWithItHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    EndWithItHandler.prototype.isSuitable = function (range, range2) {
+        return between(range2.start, range.start, range.end) && range.end === range2.end;
+    };
+    EndWithItHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range.start,
+                    end: range2.start,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return EndWithItHandler;
+}(AbstractHandler));
+exports.EndWithItHandler = EndWithItHandler;
+var BetweenRangeHandler = /** @class */ (function (_super) {
+    __extends(BetweenRangeHandler, _super);
+    function BetweenRangeHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    BetweenRangeHandler.prototype.isSuitable = function (range, range2) {
+        return (between(range2.end, range.start, range.end) && between(range2.start, range.start, range.end));
+    };
+    BetweenRangeHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range.start,
+                    end: range2.start,
+                },
+                {
+                    start: range2.end,
+                    end: range.end,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return BetweenRangeHandler;
+}(AbstractHandler));
+exports.BetweenRangeHandler = BetweenRangeHandler;
+var FullOverlapingHandler = /** @class */ (function (_super) {
+    __extends(FullOverlapingHandler, _super);
+    function FullOverlapingHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    FullOverlapingHandler.prototype.isSuitable = function (range, range2) {
+        return range2.start <= range.start && range2.end >= range.end;
+    };
+    FullOverlapingHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2) ? [] : _super.prototype.handle.call(this, range, range2);
+    };
+    return FullOverlapingHandler;
+}(AbstractHandler));
+exports.FullOverlapingHandler = FullOverlapingHandler;
+var EndOverloadHandler = /** @class */ (function (_super) {
+    __extends(EndOverloadHandler, _super);
+    function EndOverloadHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    EndOverloadHandler.prototype.isSuitable = function (range, range2) {
+        var isStartOverloaded = between(range2.end, range.start, range.end) && !between(range2.start, range.start, range.end);
+        var isEndOverloaded = !between(range2.end, range.start, range.end) && between(range2.start, range.start, range.end);
+        return !isStartOverloaded && isEndOverloaded;
+    };
+    EndOverloadHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range.start,
+                    end: range2.start,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return EndOverloadHandler;
+}(AbstractHandler));
+exports.EndOverloadHandler = EndOverloadHandler;
+var StartOverloadHandler = /** @class */ (function (_super) {
+    __extends(StartOverloadHandler, _super);
+    function StartOverloadHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    StartOverloadHandler.prototype.isSuitable = function (range, range2) {
+        var isStartOverloaded = between(range2.end, range.start, range.end) && !between(range2.start, range.start, range.end);
+        var isEndOverloaded = !between(range2.end, range.start, range.end) && between(range2.start, range.start, range.end);
+        return isStartOverloaded && !isEndOverloaded;
+    };
+    StartOverloadHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range2.end,
+                    end: range.end,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return StartOverloadHandler;
+}(AbstractHandler));
+exports.StartOverloadHandler = StartOverloadHandler;
+var NotOverlapingHandler = /** @class */ (function (_super) {
+    __extends(NotOverlapingHandler, _super);
+    function NotOverlapingHandler() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    NotOverlapingHandler.prototype.isSuitable = function (range, range2) {
+        return (!between(range2.end, range.start, range.end) && !between(range2.start, range.start, range.end));
+    };
+    NotOverlapingHandler.prototype.handle = function (range, range2) {
+        return this.isSuitable(range, range2)
+            ? [
+                {
+                    start: range.start,
+                    end: range.end,
+                },
+            ]
+            : _super.prototype.handle.call(this, range, range2);
+    };
+    return NotOverlapingHandler;
+}(AbstractHandler));
+exports.NotOverlapingHandler = NotOverlapingHandler;
+//# sourceMappingURL=handler.js.map
---
+++ dist/index.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var handler_1 = require("./handlers/handler");
+exports.remainedRange = function (rawRange, rawRange2) {
+    var isDateRange = rawRange.start instanceof Date && rawRange.end instanceof Date;
+    // if add more type, then refactor it
+    var range = isDateRange
+        ? convertDateRangeToNumberRange(rawRange)
+        : rawRange;
+    var range2 = isDateRange
+        ? convertDateRangeToNumberRange(rawRange2)
+        : rawRange2;
+    var remainedRanges = calculateRemainedRange(range, range2);
+    // if add more type, then refactor it
+    return isDateRange
+        ? remainedRanges.map(function (remainedRange) { return convertNumberRangeToDateRange(remainedRange); })
+        : remainedRanges;
+};
+var convertDateRangeToNumberRange = function (range) { return ({
+    end: range.end.getTime(),
+    start: range.start.getTime(),
+}); };
+var convertNumberRangeToDateRange = function (range) { return ({
+    end: new Date(range.end),
+    start: new Date(range.start),
+}); };
+var calculateRemainedRange = function (range, range2) {
+    var sameRange = new handler_1.SameRangeHandler();
+    var startWithIt = new handler_1.StartWithItHandler();
+    var endWithIt = new handler_1.EndWithItHandler();
+    var betweenRange = new handler_1.BetweenRangeHandler();
+    var fullOverlaping = new handler_1.FullOverlapingHandler();
+    var endOverload = new handler_1.EndOverloadHandler();
+    var startOverload = new handler_1.StartOverloadHandler();
+    var notOverlaping = new handler_1.NotOverlapingHandler();
+    sameRange
+        .setNext(new handler_1.StartWithItHandler())
+        .setNext(new handler_1.EndWithItHandler())
+        .setNext(new handler_1.BetweenRangeHandler())
+        .setNext(new handler_1.FullOverlapingHandler())
+        .setNext(new handler_1.EndOverloadHandler())
+        .setNext(new handler_1.StartOverloadHandler())
+        .setNext(new handler_1.NotOverlapingHandler());
+    return sameRange.handle(range, range2);
+};
+var createDate = function (hour, minutes) {
+    var d = new Date();
+    d.setUTCHours(hour, minutes, 0, 0);
+    return d;
+};
+console.log(exports.remainedRange({ end: createDate(20, 0), start: createDate(2, 0) }, { end: createDate(17, 0), start: createDate(16, 0) }));
+//# sourceMappingURL=index.js.map
